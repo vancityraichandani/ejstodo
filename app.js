12e6a2fb499317528d8f605e56987fb4cfb16b90
vancityraichandani <someshraichandani1@gmail.com>
2022-11-16 00:19:53 +0530

    refactored with module exports

--- app.js
+++ app.js
@@ -1,35 +1,28 @@
 const express = require("express")
 const bodyParser = require("body-parser")
+const dateMod = require(__dirname + "/date.js")
 
 const app = express()
-var items = ["buy", "eat", "cook"]
+const items = ["buy", "eat", "cook"]
 
 app.set("view engine", "ejs")
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static("public"))
 
 app.get("/", function (req, res) {
 
-    var today = new Date()
+    const date = dateMod.getDate()
 
-    var options = {
-        weekday: 'long',
-        day: 'numeric',
-        month: 'long'
-    }
-
-    var date = today.toLocaleDateString("hi-IN", options)
-
-    res.render('index', {date: date, taskArr: items});
+    res.render('index', { date: date, taskArr: items });
 })
 
-app.post("/", function(req, res){
-    var item = req.body.newItem;
+app.post("/", function (req, res) {
+    const item = req.body.newItem;
     items.push(item);
 
     res.redirect("/");
 })
 
-app.listen(3000, function(){
+app.listen(3000, function () {
     console.log("Server up");
 })
